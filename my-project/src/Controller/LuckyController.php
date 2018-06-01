493e5edaa43a29007de9bbf78b0302ccee2fa165
majdarbash <majd.arbash@gmail.com>
2018-06-01 07:59:58 +0400

    +[MyProject][LuckyController]: using annotated route and routes.yaml defined path simultaneously

--- my-project/src/Controller/LuckyController.php
+++ my-project/src/Controller/LuckyController.php
@@ -7,15 +7,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController
 {
-    /**
-     * @Route("/lucky/number")
-     */
+
     public function number()
     {
         $number = mt_rand(0, 100);
         return new Response(
             '<html><body>Your lucky number is: ' . $number . '</body></html>'
         );
+    }
+
+    /**
+     * @Route("/lucky/number-annotated")
+     */
+    public function numberRouteAnnotated()
+    {
+        return $this->number();
     }
 
 }
